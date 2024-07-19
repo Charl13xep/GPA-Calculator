27b56431c60bcd23c9053f1ecc916a90c7481cc7
Charl13xep <104887044+Charl13xep@users.noreply.github.com>
2024-07-19 22:42:52 +0300

    Update firebaseauth.js
    Removed google api key for security

--- firebaseauth.js
+++ firebaseauth.js
@@ -4,7 +4,7 @@ import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} fro
 import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 const firebaseConfig = {
-    apiKey: "AIzaSyD1JMW5BK_DHR_Hi2iLZsa6Xt0dmnKZ9TM",
+    apiKey: "",
     authDomain: "gpa-calculator-60283.firebaseapp.com",
     projectId: "gpa-calculator-60283",
     storageBucket: "gpa-calculator-60283.appspot.com",
